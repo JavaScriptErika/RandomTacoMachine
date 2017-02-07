ce7063a0d01272a74f16ddc1d23295666d64803a
Erika Ritter <ErikaJRitter@gmail.com>
2017-02-06 19:08:07 -0500

    Add comments

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
 const tacoAPI = "http://taco-randomizer.herokuapp.com/random/";
+//empty object initialized so when JSON object is returned, can be accessed globally
 let tacoObject = {};
+//element id is selected for full description of the recipe
 let $tacoFull = $('#tacoFull');
 const $recipeButton = $('#recipeButton');
 const $nextTaco = $('#nextTacoButton');
 
-
+//callback function for ajaxRequest() that gives a description of taco recipe
+//data is what is returned from the request as a JSON object
 function tacoSummary(data) {
 	tacoObject = data;
 	let $tacoP = $('#tacoP');
 	let tacoDescription = "";
 
-	tacoDescription += "A yummy taco with " + '<b>' + tacoObject.mixin.name + '</b> ';
-	tacoDescription += "and lots of flavor thanks to " + '<b>' + tacoObject.seasoning.name + '</b>. ';
-	tacoDescription += "Stuff your face with a big boy portion of " + '<b>' + tacoObject.base_layer.name + '</b>! ';
+	tacoDescription += 'A yummy taco with ' + '<b>' + tacoObject.mixin.name + '</b> ';
+	tacoDescription += 'and lots of flavor thanks to ' + '<b>' + tacoObject.seasoning.name + '</b>. ';
+	tacoDescription += 'Stuff your face with a big boy portion of ' + '<b>' + tacoObject.base_layer.name + '</b>! ';
 	tacoDescription += 'Don\'t forget the ' + '<b>' + tacoObject.condiment.name + '</b>. ';
-	tacoDescription += "Best part? It's all tucked away in " + '<b>' + tacoObject.shell.name + '</b>.';
+	tacoDescription += 'Best part? It\'s all tucked away in ' + '<b>' + tacoObject.shell.name + '</b>.';
 
 	tacoToHTML($tacoP, tacoDescription);
 	tacoTweet(tacoObject);
 
 } // end tacoSummary()
 
-function tacoTweet(tacoObject) {
+//converts markdown to html formatting
+function tacoToHTML(htmlElement, tacoMdFormat) {
+	const converter = new showdown.Converter();
+	let tacoConverted = converter.makeHtml(tacoMdFormat);
+	htmlElement.html(tacoConverted);
+}
 
-	$twitterButton = $('#twitterButton');
+//grabs tacoObject to iterate through ingredients to tweet
+function tacoTweet(tacoIngredients) {
+
+	const $twitterButton = $('#twitterButton');
 	let tacoName = "";
 
-	$.each(tacoObject, function (i) {
+	$.each(tacoIngredients, function (i) {
 
 		tacoName += this.name + ', ';
 	})
@@ -37,28 +48,21 @@ function tacoTweet(tacoObject) {
 	$twitterButton.attr('href', url);
 
 }
-
-
-function tacoToHTML(htmlElement, tacoMdFormat) {
-	const converter = new showdown.Converter();
-	let tacoConverted = converter.makeHtml(tacoMdFormat);
-	htmlElement.html(tacoConverted);
-}
-
+//AJAX request with URL and callback function
 function ajaxRequest() {
 	$.getJSON(tacoAPI, tacoSummary);
-
 } //end ajaxRequest()
 
-
+//when page loads, a recipe will already be available
 window.onload = function () {
 	ajaxRequest();
+	//hides the summary to begin with
 	$tacoFull.hide();
-
 }; //end onload event
 
 $nextTaco.on('click', function () {
 	ajaxRequest();
+	//hides summary when user clicks next taco button
 	$tacoFull.hide();
 }); // end click event
 
